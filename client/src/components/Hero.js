2d38b75b114d39e998f130bb9b574760ceafe9b9
Gerardo Paredes <gerardparedes23@gmail.com>
2020-02-05 22:14:51 -0800

    Hero component added

---
+++ client/src/components/Hero.js
@@ -0,0 +1,18 @@
+// importing modules
+import React from 'react';
+
+// importing stylesheet
+import './Hero.css';
+
+const hero = () => (
+    <div className="hero">
+      <img 
+        alt="chuck-norris-img"
+        className="hero__img"
+        src="https://www.clipartkey.com/mpngs/m/277-2778119_
+            chuck-norris-chuck-norris-cartoon-pics-transparent.png"
+        />
+    </div>
+);
+
+export default hero;
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,15 +1,36 @@
+// importing modules
 import React, { Component } from 'react';
+
+// importing components
+import Hero from '../components/Hero';
+
+// importing stylesheet
+import './Landing.css';
 
 class LandingPage extends Component {
   constructor(props){
     super(props);
-    this.state = {};
+    this.state = {
+      joke: ''
+    };
   }
+
+  componentDidMount(){
+
+  }
+
+  handleRandomJoke = () => {
+
+  };
+
   render(){
     return (
-      <div>LANDING PAGE</div>
+      <div className="landingPage">
+        <Hero />
+      </div>
     );
   }
 };
 
 export default LandingPage;
+
